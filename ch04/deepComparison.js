7eca61f8e5181f314589f3df359516996b18250f
Tiago Lisboa Alves <tiago.alves@serpro.gov.br>
2017-09-01 18:30:44 -0300

    fix bug in deepComparison solution: simple values wasn't being compared

--- ch04/deepComparison.js
+++ ch04/deepComparison.js
@@ -1,31 +1,36 @@
-function getSubObjects(objA){
+function hasSubObjects(objA){
     return Object.keys(objA).filter(function(property){
        return typeof objA[property] === "object" && objA[property] !== null;       
     });
 }
 
+function parseToObject(value){    
+    return typeof value === "object" ? value : {value : value};
+}
+
 function deepEqual(objA, objB){
-   if(!getSubObjects(objA)[0]){
+   if(!hasSubObjects(objA)[0]){
         return Object.keys(objA).reduce(function(result, property){
             return result && objA[property] === (objB || {})[property]; 
         }, true);
     } else {
-        return getSubObjects(objA).reduce(function(result, property, index){
-            return result && deepEqual(objA[getSubObjects(objA)[index]], (objB || {})[getSubObjects(objA)[index]]);
+        return Object.keys(objA).reduce(function(result, property){
+            return result && deepEqual(parseToObject(objA[property]), parseToObject(objB[property]));
         }, true);
     }
 }
 
 
 console.log(deepEqual({
-    a : 1, 
+    a : 4, 
     b : {
         f : 1,
         d :  {
             c : 1
         }
     },
-    x : {
+    z : 5,
+    s : {
         d :  {
             c : 4,
             s : {
@@ -35,13 +40,14 @@ console.log(deepEqual({
     }
 }, 
 {
-    a : 1, 
+    a : 4, 
     b : {
         f : 1,
         d :  {
             c : 1
         }
     },
+    z : 5,
     s : {
         d :  {
             c : 4,
